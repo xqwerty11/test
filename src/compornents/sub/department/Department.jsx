784a1e9a8f55d8517b42e917296a1fb1f1d8c7c5
xqwerty11 <hk637999@gmail.com>
2023-10-04 09:50:10 +0900

    module.scss 주석 추가

--- src/compornents/sub/department/Department.jsx
+++ src/compornents/sub/department/Department.jsx
@@ -1,11 +1,18 @@
 import Layout from '../../common/layout/Layout';
 // import styles from './Department.module.scss';
 // import clsx from 'clsx';
+import styles from './Department.module.scss';
 
 export default function Department() {
 	return (
 		<Layout title={'Department'}>
 			<p>디파트먼트 페이지입니다</p>
+			<ul>
+				<li className={styles.abc}>list1</li>
+				<li>list2</li>
+				<li>list3</li>
+				<li>list4</li>
+			</ul>
 		</Layout>
 	);
 }
