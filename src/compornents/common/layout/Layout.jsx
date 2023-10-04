20d60308117b032677d16153ea2ac291650b3211
xqwerty11 <hk637999@gmail.com>
2023-10-04 15:17:48 +0900

    딜레이 모션 오류버전

--- src/compornents/common/layout/Layout.jsx
+++ src/compornents/common/layout/Layout.jsx
@@ -8,15 +8,17 @@ export default function Layout({ title, children, styleName }) {
 	const tit = useRef(null);
 
 	//인수로 참조객체를 받아서 참조객체의 글자값을 반복돌면서 span으로 감싸주는 함수
-	const splitText = (ref) => {
+	const splitText = (ref, gap = 0.1, delay = 0) => {
 		//문자열이 담길 변수 초기화
 		let tags = '';
-
+		let count = 0;
 		//참조객체안쪽의 글자를 innerText로 구해서
 		//for of로 반복처리하면서 span태그로 감싸줌
 		for (let letter of ref.current.innerText) {
-			tags += `<span>${letter}</span>`;
+			tags += `<span style='transition-delay:${gap * count + delay}s'>${letter}</span>`;
+			count++;
 		}
+
 		//기존 참조객체 안쪽에 글자는 지워주고
 		ref.current.innerText = '';
 		//span으로 감싸준 문자열을 참조객체의 innerHTML으로 삽입
@@ -25,7 +27,8 @@ export default function Layout({ title, children, styleName }) {
 
 	useEffect(() => {
 		setIsOn(true);
-		splitText(tit);
+		//컴포넌트가 마운트되자마자 h1요소가 담겨있는 innerHTML으로 삽입
+		splitText(tit, 0.1, 0.5);
 	}, []);
 
 	return (
